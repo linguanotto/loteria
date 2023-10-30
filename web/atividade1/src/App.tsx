f61fcf2766cc46466cb8fe41b70dbd619cb37f93
Linguanotto <flavioeduardolinguanotto@gmail.com>
2023-10-30 20:55:34 -0300

    2

--- web/atividade1/src/App.tsx
+++ web/atividade1/src/App.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import loteria from "./services/Loteria";
+
 export default  function App() {
+  const [megasena,setMegasena] = useState({});
+  const [lotofacil,setLotofacil] = useState({});
+
+  function testar(){
+    loteria.get().then(r => {
+      setMegasena (r.megasena);
+      setLotofacil(r.lotofacil);
+        })
+    .catch(e => console.log(e.message));
+    }
+  useEffect(testar,[]);
+
   return (
     <div>
       oi
